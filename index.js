9d06c6fc3f1db687b855f0465cdeadf2b2892d48
Your Name <you@example.com>
2015-12-04 16:40:53 +0300

    enable view to allow user post movie and save this information to the database{post and get method}

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose.connect('mongodb://localhost/project-aardvark');
 //here at the schema is where you add fields and their rules for your databases
 var movieSchema = mongoose.Schema({
     title: String,
-    year_of_release: Number,
+    // year_of_release: Number,
     category: String,
     rating: {
         type: Number,
@@ -58,6 +58,14 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+//new route this get request handles post request
+//the code below allows user to get posted movies
+app.get('/movies/new', function(req, res) {
+    
+
+                res.render('new');
+            });
+
 //routing using express easy
 //adding jade template 
 app.get('/movies', function(req, res) {
@@ -79,70 +87,7 @@ app.get('/movies', function(req, res) {
 });
 //res.json(movies);
 
-
-// movies =[ 
-// {
-//  title: 'Black Mass',
-//  category: ['thriller', 'action', 'biopic'],
-//  main_actors: [
-//  {
-//   first_name: 'Johnny',
-//   last_name: 'Depp'
-//  },  
-//  {
-//    first_name: 'Benedict',
-//    last_name: 'Cumberbatch'
-
-
-//  }]
-
-//  },
-
-// {
-//  title: 'Avengers: age of Ultron',
-//  category: ['fantasy', 'action'],
-//  main_actors: [
-//  {
-//  first_name: 'Chris',
-//  last_name: 'Evans'
-
-
-//  }, 
-//  {
-//  first_name: 'Robert',
-//  last_name: 'Jr'
-
-
-
-//  }]
-
-
-// },
-// {
-//  title: 'Straight-Outta-Compton',
-//  category: ['drama' , ''],
-//  main_actors: [
-//  {
-//  first_name: 'Oshea',
-//  last_name: 'Jackson'
-
-
-
-//   }, 
-//   {
-
-// first_name: 'aldis',
-// last_name: 'Hodge'
-
-
-
-//  }
-//  ]
-
-
-// }
-
-// ];
+    
 
 
 
@@ -150,8 +95,8 @@ app.get('/movies', function(req, res) {
 
 
 
-
-app.post('/movies/new', function(req, res) {
+//this code allows user to post info to the db 
+app.post('/movies', function(req, res) {
     console.log(req.body);
     formdata = req.body;
     //    //create an instance of a movie
@@ -179,144 +124,7 @@ app.post('/movies/new', function(req, res) {
 
 });
 
-//dispatch is a route handler 
-// var server = http.createServer(
-//      dispatch({
 
-//        '/movies' : {
-
-//          'GET /' : function(request, response){
-//                     movies =[ 
-//                     {
-//                      title: 'Black Mass',
-//                      category: ['thriller', 'action', 'true-story'],
-//                      main_actors: [
-//                      {
-//                       first_name: 'Johnny',
-//                       last_name: 'Depp'
-//                      },  
-//                      {
-//                        first_name: 'Benedict',
-//                        last_name: 'Cumberbatch'
-
-
-//                      }]
-
-//                      },
-
-//                     {
-//                      title: 'Avengers: age of Ultron',
-//                      category: ['fanatsy', 'action'],
-//                      main_actors: [
-//                      {
-//                      first_name: 'Chris',
-//                      last_name: 'Evans'
-
-
-//                      }, 
-//                      {
-//                      first_name: 'Robert',
-//                      last_name: 'Jr'
-
-
-
-//                      }]
-
-
-//                     },
-//                     {
-//                      title: 'Straight-Outta-Compton',
-//                      category: ['drama' , ''],
-//                      main_actors: [
-//                      {
-//                      first_name: 'Oshea',
-//                      last_name: 'Jackson'
-
-
-
-//                       }, 
-//                       {
-
-//                     first_name: 'aldis',
-//                     last_name: 'Hodge'
-
-
-
-//                      }
-//                      ]
-
-
-//                     }
-
-//                     ];
-
-
-//                     response.end(JSON.stringify(movies));
-//          },
-//         '/POST': function(request, response, next){
-//          //get parameters from the form 
-//        var  formdata;
-//          request.on('data', function(chunk){
-//             formdata = querystring.parse(chunk.toString());
-
-//          });
-
-//            request.on('end', function(){
-//         console.log(formdata);
-//         //create an instance of a movie
-//         var movie = new Movie(
-//         {
-//           title: formdata.title,
-//           year_of_release: formdata.year_of_release
-
-
-
-//         }
-//         );
-
-
-
-
-//            });
-
-
-//           //syntax for creation of a new instance ...... var name = new name({})
-
-
-
-//         }
-
-
-//        }
-//    })
-//      );
-
-
-// console.log('visiting %s', request.url);
-// Access-Control-Allow-Origin allows browser to be accessed from other pages 
-//    response.writeHead(200,{
-//       'content-type' : 'application/json',
-//       ' Access-Control-Allow-Origin' : '*'
-
-
-
-//    });
-
-
-
-// response.end(JSON.stringify(message));
-
-// //  },
-// //  '/movies' : function (request,response){
-
-// // console.log('visiting %s', request.url);
-// // response.end('this is the movies path');
-
-// //  },
-// //   '/actors' : function (request,response){
-
-// // console.log('visiting %s', request.url);
-// // response.end('this is the actors path');
 app.get('/movies/:id', function(req, res) {
     movieId = req.params.id;
 
