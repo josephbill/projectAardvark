3192facae4bceeabbd9a798047e8cbcb6e46a1de
Your Name <you@example.com>
2015-12-15 14:44:13 +0300

    passport authentication,log in ,log out ,routes,and other fun stuff to learn

--- index.js
+++ index.js
@@ -1,256 +1,66 @@
-//Including Express
+//Express
 var express = require('express');
-//including consolidate 
-var cons = require('consolidate');
 var app = express();
-//allowing cross origin resource sharing 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+var cons = require('consolidate');
+var http = require('http');
 
-//including express middleware
-//body-parser 
+//passport
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
+//Express Middleware
 var bodyParser = require('body-parser');
 
-
-
-
-//include mongoose 
-//mongoose allows validation of entries in database 
+//Include Mongoose
 var mongoose = require('mongoose');
-
 mongoose.connect('mongodb://localhost/project-aardvark');
 
-
-//define our schema
-//here at the schema is where you add fields and their rules for your databases
-var movieSchema = mongoose.Schema({
-    title: String,
-    year_of_release: Number,
-    category: String,
-    rating: {
-        type: Number,
-        default: 0,
-        min: 0,
-        max: 10
-
-    }
-
-
+//Allow CORS
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Accept');
+    next();
 });
-//compile our model
-//informs mongodb available
-var Movie = mongoose.model('Movie', movieSchema);
 
-//express settings
-//intiates template engines  
+//Express settings
 app.engine('html', cons.liquid);
-//including jade templating engine 
-//the first code puts the path of the jade file
+
 app.set('views', './views');
-//initiates jade template engine 
-//app.set('view engine', 'jade');
-app.set('view engine', 'html')
-    //(express)adding the middleware
+app.set('view engine', 'html');
+
+
+//Express Middleware
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-// app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressSession({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
+//Include routes
+var moviesRoutes = require('./routes/movies');
+var usersRoutes = require('./routes/user')
+var index1Route = require ('./routes/index')
+app.use(moviesRoutes);
+app.use(usersRoutes);
+app.use(index1Route);
 
+// passport config
+var User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-//routing using express easy
-//adding jade template 
-app.get('/movies', function(req, res) {
-    //the following code directs filter actions 
-    Movie.find()
-        .select('title year_of_release rating')
-        .exec(function(err, movies) {
-            if (err) {
-
-
-                console.log(err);
-
-            } else {
-                // res.json(movies);
-                 res.render('index', {'movies': movies});
-            }
-        });
-
-});
-//res.json(movies);
-
-
-// //new route this get request handles post request
-// //the code below allows user to get posted movies
-app.get('/movies/new', function(req, res) {
-
-
-    res.render('new');
-});
-
-
-
-
-
-
-//this code allows user to post info to the db 
-app.post('/movies', function(req, res) {
-    console.log(req.body);
-    formdata = req.body;
-    //    //create an instance of a movie
-    // var movie = new Movie(
-    // {
-    //   title: formdata.title,
-    //   year_of_release: formdata.year_of_release
-    // }
-    // );
-    //the code commented below imports data from the mongodb onto the view of the app 
-    //thus the code above is rendered useless if the code below is initialized
-    var movie = new Movie(formdata);
-    movie.save(function(err, movie) {
-        if (err) {
-
-            console.log(err);
-        } else {
-            console.log('succesfully saved the movie :-)');
-            //
-            res.redirect('/movies');
-        }
-
-    });
-
-
-});
-
-
-app.get('/movies/:id', function(req, res) {
-    movieId = req.params.id;
-
-
-    //retrieve data from the mongodb
-    Movie.findById(movieId, function(err, movie) {
-        if (err) return console.log(err);
-
-        res.render('detail', {
-            "movie": movie
-        });
-        // res.json(movie);
-    });
-});
-
-
-app.get('/movies/:id/edit', function(req, res) {
-    movieId = req.params.id;
-
-    // retrieve the movie from Mongodb
-    Movie.findById(movieId, function(err, movie) {
-        if (err) return console.log(err);
-        res.render('edit', {
-            "movie": movie
-        });
-        // res.json(movie);
-    });
-});
-
-
-//put verb....... updating data from the database
-// app.put('/movies/:id', function(req, res) {
-//     movieId = req.params.id;
-//     userRating = req.body.rating;
-
-//     //retrieve data from the mongodb
-//     Movie.findById(movieId, function(err, movie) {
-//         if (err) return console.log(err);
-//         movie.rating = userRating;
-//         movie.save(function(err, movie) {
-//             if (err) return console.log(err);
-//             res.json(movie);
-//         });
-
-
-//     });
-// });
-
-function updateMovie(method, req, res) {
-    movieId = req.params.id;
-
-    userRating = req.body.rating;
-    userTitle = req.body.title;
-    userYearOfRelease = req.body.year_of_release;
-   
-
-    // retrieve the movie from Mongodb
-    Movie.findById(movieId, function(err, movie) {
-        if (err) return console.log(err);
-
-        movie.rating = userRating;
-        movie.title = userTitle;
-        movie.year_of_release = userYearOfRelease;
-       
-
-        movie.save(function(err, movie) {
-            if (err) return console.log(err);
-
-            if (method === 'PUT') {
-                res.json(movie);
-            } else {
-                res.redirect('/movies/' + movie._id);
-            };
-        });
-    });
-}
-
-
-
-app.post('/movies/:id/edit', function(req, res) {
-
-    updateMovie('POST', req, res);
-});
-
-app.put('/movies/:id', function(req, res) {
-    updateMovie('PUT', req, res);
-});
-
-function deleteMovie(method, req, res) {
-    movieId = req.params.id;
-
-    // retrieve the movie from Mongodb
-    Movie.remove({
-        _id: movieId
-    }, function(err) {
-        if (err) return console.log(err);
-
-        if (method === 'GET') {
-            res.redirect('/movies');
-        } else {
-            res.send('Movie was deleted');
-        };
-    });
-}
-
-app.get('/movies/:id/delete', function(req, res) {
-    deleteMovie('GET', req, res);
-});
-
-
-
-
-
-
-//delete verb .... to delete data from the database
-app.delete('/movies/:id', function(req, res) {
-    deleteMovie('DELETE', req, res);
-});
-
-
-
-app.listen(8085, function() {
-    console.log('server runnning on http://127.0.0.1:8085');
-
-
-
+// console.log(routes);
+app.listen(8081, function() {
+    console.log('server running on http://127.0.0.1:8081');
 });
